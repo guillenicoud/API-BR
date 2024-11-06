54c8cfff41bec67be5cc2d3f72692c8d372f3f33
Guillermo Nicoud <132966306+guillenicoud@users.noreply.github.com>
2024-11-05 23:06:44 -0300

    agrego de try-catch en controllers

--- src/controllers/empresa.controller.js
+++ src/controllers/empresa.controller.js
@@ -1,35 +1,37 @@
 import { pool } from "../db/postgreSQL.js";
 
 export const getEmpresas = async (req, res) => {
-
-    const { rows } = await pool.query('SELECT * FROM empresas');
-
-    res.json(rows);
+    try {
+        const { rows } = await pool.query('SELECT * FROM empresas');
+        res.json(rows);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export const getEmpresa = async (req, res) => {
-
-    const { id } = req.params; 
-    
-    const { rows } = await pool.query('SELECT * FROM empresas WHERE "idEmpresa" = ($1)',[id]);
-        
-    res.json(rows);
+    try {
+        const { id } = req.params; 
+        const { rows } = await pool.query('SELECT * FROM empresas WHERE "idEmpresa" = ($1)',[id]);
+        res.json(rows);    
+    } catch (error) {
+        console.log(error);
+    }    
 }
 
 export const postEmpresa = async (req, res) => {
-    
-    const { empresa } = req.body;
-
-    const { rows } = await pool.query('INSERT INTO empresas (empresa) VALUES ($1)', [empresa]);
-
-    res.json(rows);
+    try {
+        const { empresa } = req.body;
+        const { rows } = await pool.query('INSERT INTO empresas (empresa) VALUES ($1)', [empresa]);
+        res.json(rows);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export const deleteEmpresa = async (req, res) => {
-
-    const { id } = req.params;
-
     try {
+        const { id } = req.params;
         const { rows } = await pool.query('DELETE FROM empresas WHERE "idEmpresa" = ($1)',  [id])
         res.json(rows);
 
